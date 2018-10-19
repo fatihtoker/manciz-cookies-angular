c10ed5ea6d3aadfdaa60c9779605d29676479ae8
Fatih Toker <fatihtoker@anadolu.edu.tr>
2018-10-19 15:33:35 +0300

    spotify frame added.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, transition, useAnimation, state, style, animate} from '@angular/animations';
+import {fadeInDown, fadeInUp} from 'ng-animate';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  animations: [
+    trigger('status', [
+      state('collapsed', style({
+        display: 'none',
+        opacity: 0,
+        transform: 'translateY(5%)',
+      })),
+      state('expanded', style({
+        display: 'block',
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      transition('collapsed => expanded', animate('450ms ease-out')),
+      transition('expanded  => collapsed', animate('450ms ease-in'))
+    ])
+  ]
 })
 export class LandingComponent implements OnInit {
-
+  hidden = true;
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  onIconClicked() {
+    this.hidden = !this.hidden;
   }
 
 }
